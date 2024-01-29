07160f037f6f59471ef63e7b0450117e8d28431f
margheritabattistotti <62936400+margheritabattistotti@users.noreply.github.com>
2024-01-29 17:21:56 +0100

    Update MCTS_main.py
    Correct indentation

--- Scripts/MCTS_main.py
+++ Scripts/MCTS_main.py
@@ -23,45 +23,45 @@ def throwing_success(c0, c1):
 
 # ################################################# FORWARD PASS ################################################# #
 
-    def forward_pass(initial_s):
-        # Defines the optimal strategy to complete the mission following a MonteCarlo Tree Search starting
-        # from 'initial_s',
-        # OUTPUT
-        # action_sequence: sequence of optimal actions
-        # states_sequence: optimal state sequence
-        s = MT.MCTSPreDecisionNode(initial_s)
-        objective = 0
-        action_sequence = []
-        state_sequence = [s.state]
-        while not MR.is_terminal(s.state):
-            best_action_type, best_a = s.MCTS_best_action()
-            if best_action_type == 'throw':
-                best_action = best_action_type + ' ' + str(best_a)
-                p_success = throwing_success(MR.nodes_coordinates[s.state[1]], MR.trays_coordinates[MR.trays[best_a[1]]])
-                if random.random() > p_success:
-                    best_new_state = MR.failed_new_state_throwing(best_a, s.state)
-                    best_action = best_action + ' ' + '(FAILURE)'
-                else:
-                    best_new_state = MR.new_state_throwing(best_a, s.state)
-            elif best_action_type == 'move':
-                best_action = best_action_type + ' ' + str(best_a)
-                p_risk = MR.nodes_connections[(s.state[1], best_a)]['risk'] / 100
-                if random.random() < p_risk:
-                    best_new_state = MR.failed_new_state_moving(best_a, s.state)
-                    best_action = best_action + ' ' + '(COLLISION)'
-                else:
-                    best_new_state = MR.new_state_moving(best_a, s.state)
+def forward_pass(initial_s):
+    # Defines the optimal strategy to complete the mission following a MonteCarlo Tree Search starting
+    # from 'initial_s',
+    # OUTPUT
+    # action_sequence: sequence of optimal actions
+    # states_sequence: optimal state sequence
+    s = MT.MCTSPreDecisionNode(initial_s)
+    objective = 0
+    action_sequence = []
+    state_sequence = [s.state]
+    while not MR.is_terminal(s.state):
+        best_action_type, best_a = s.MCTS_best_action()
+        if best_action_type == 'throw':
+            best_action = best_action_type + ' ' + str(best_a)
+            p_success = throwing_success(MR.nodes_coordinates[s.state[1]], MR.trays_coordinates[MR.trays[best_a[1]]])
+            if random.random() > p_success:
+                best_new_state = MR.failed_new_state_throwing(best_a, s.state)
+                best_action = best_action + ' ' + '(FAILURE)'
             else:
-                best_action = best_action_type + ' ' + str(best_a)
-                best_new_state = MR.new_state_picking(best_a, s.state)
+                best_new_state = MR.new_state_throwing(best_a, s.state)
+        elif best_action_type == 'move':
+            best_action = best_action_type + ' ' + str(best_a)
+            p_risk = MR.nodes_connections[(s.state[1], best_a)]['risk'] / 100
+            if random.random() < p_risk:
+                best_new_state = MR.failed_new_state_moving(best_a, s.state)
+                best_action = best_action + ' ' + '(COLLISION)'
+            else:
+                best_new_state = MR.new_state_moving(best_a, s.state)
+        else:
+            best_action = best_action_type + ' ' + str(best_a)
+            best_new_state = MR.new_state_picking(best_a, s.state)
 
-            state_sequence.append(best_new_state)
-            action_sequence.append(best_action)
-            s = MT.MCTSPreDecisionNode(best_new_state)
+        state_sequence.append(best_new_state)
+        action_sequence.append(best_action)
+        s = MT.MCTSPreDecisionNode(best_new_state)
 
-        objective += MR.terminal_state_evaluation(s.state)
+    objective += MR.terminal_state_evaluation(s.state)
 
-        return action_sequence, state_sequence, objective
+    return action_sequence, state_sequence, objective
 
 
 # ################################################# MAIN EXECUTION ################################################# #
